9c5a0a563b0ea46e08c3965a360b4c9d79fa268c
VIPIN SINGH MEHRA <vipinsinghmehra535@gmail.com>
2023-08-25 12:22:43 +0530

    serach user bug fixed

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:instagram_clone_flutter/screens/profile_screen.dart';
 import 'package:instagram_clone_flutter/utils/color.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -24,6 +25,7 @@ class _SearchScreenState extends State<SearchScreen> {
             controller: searchController,
             decoration: const InputDecoration(labelText: 'Search'),
             onFieldSubmitted: (String _) {
+              print("sub ${_}");
               setState(() {
                 isShowUsers = true;
               });
@@ -37,42 +39,88 @@ class _SearchScreenState extends State<SearchScreen> {
                   .collection('users')
                   .where(
                     'username',
-                    isGreaterThanOrEqualTo: searchController.text,
+                    isGreaterThanOrEqualTo: searchController.text.toUpperCase(),
                   )
                   .get(),
               builder: (context, snapshot) {
-                if (!snapshot.hasData) {
+                if (snapshot.connectionState == ConnectionState.none) {
                   return const Center(
                     child: CircularProgressIndicator(),
                   );
-                }
-                return ListView.builder(
-                  itemCount: (snapshot.data! as dynamic).docs.length,
-                  itemBuilder: (context, index) {
-                    return InkWell(
-                      // onTap: () => Navigator.of(context).push(
-                      //   MaterialPageRoute(
-                      //     builder: (context) => ProfileScreen(
-                      //       uid: (snapshot.data! as dynamic).docs[index]['uid'],
-                      //     ),
-                      //   ),
-                      // ),
-                      child: ListTile(
-                        // leading: CircleAvatar(
-                        //   backgroundImage: NetworkImage(
-                        //     (snapshot.data! as dynamic).docs[index]['photoUrl'],
-                        //   ),
-                        //   radius: 16,
-                        // ),
-                        title: Text(
-                          (snapshot.data! as dynamic).docs[index]['username'],
-                        ),
-                      ),
+                } else if (snapshot.connectionState ==
+                    ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else {
+                  if (!snapshot.hasData) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
                     );
-                  },
-                );
+                  }
+                  return ListView.builder(
+                    itemCount: (snapshot.data! as dynamic).docs.length,
+                    itemBuilder: (context, index) {
+                      return InkWell(
+                        onTap: () => Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => Profile(
+                              uid: (snapshot.data! as dynamic).docs[index]
+                                  ['uid'],
+                            ),
+                          ),
+                        ),
+                        child: ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage: NetworkImage(
+                              (snapshot.data! as dynamic).docs[index]
+                                  ['photoUrl'],
+                            ),
+                            radius: 16,
+                          ),
+                          title: Text(
+                            (snapshot.data! as dynamic).docs[index]['username'],
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                }
+
+                // if (!snapshot.hasData) {
+                //   return const Center(
+                //     child: CircularProgressIndicator(),
+                //   );
+                // }
+                // return ListView.builder(
+                //   itemCount: (snapshot.data! as dynamic).docs.length,
+                //   itemBuilder: (context, index) {
+                //     return InkWell(
+                //       // onTap: () => Navigator.of(context).push(
+                //       //   MaterialPageRoute(
+                //       //     builder: (context) => ProfileScreen(
+                //       //       uid: (snapshot.data! as dynamic).docs[index]['uid'],
+                //       //     ),
+                //       //   ),
+                //       // ),
+                //       child: ListTile(
+                //         leading: CircleAvatar(
+                //           backgroundImage: NetworkImage(
+                //             (snapshot.data! as dynamic).docs[index]['photoUrl'],
+                //           ),
+                //           radius: 16,
+                //         ),
+                //         title: Text(
+                //           (snapshot.data! as dynamic).docs[index]['username'],
+                //         ),
+                //       ),
+                //     );
+                //   },
+                // );
               },
             )
+          // :
+          //  Container());
           : FutureBuilder(
               future: FirebaseFirestore.instance
                   .collection('posts')
--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,176 +1,231 @@
-import 'package:flutter/foundation.dart';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:instagram_clone_flutter/resources/auth_methods.dart';
 import 'package:instagram_clone_flutter/responsive/mobile_screen_layout.dart';
+
 import 'package:instagram_clone_flutter/responsive/responsive_layout_srceen.dart';
 import 'package:instagram_clone_flutter/responsive/web_screen_layout.dart';
+import 'package:instagram_clone_flutter/screens/login_screen.dart';
 import 'package:instagram_clone_flutter/utils/color.dart';
+
 import 'package:instagram_clone_flutter/utils/utils.dart';
 import 'package:instagram_clone_flutter/widgets/text_field_input.dart';
 
-class SignUpScreen extends StatefulWidget {
-  const SignUpScreen({Key? key}) : super(key: key);
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({Key? key}) : super(key: key);
 
   @override
-  State<SignUpScreen> createState() => _SignUpScreenState();
+  State<SignupScreen> createState() => _SignupScreenState();
 }
 
-class _SignUpScreenState extends State<SignUpScreen> {
+class _SignupScreenState extends State<SignupScreen> {
+  final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _bioController = TextEditingController();
-  final TextEditingController _usernameController = TextEditingController();
+  bool _isLoading = false;
   Uint8List? _image;
-  bool isLoading = false;
 
   @override
   void dispose() {
+    super.dispose();
     _emailController.dispose();
     _passwordController.dispose();
     _usernameController.dispose();
-    _bioController.dispose();
-    super.dispose();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    final size = MediaQuery.sizeOf(context);
+  void signUpUser() async {
+    // set loading to true
+    setState(() {
+      _isLoading = true;
+    });
 
-    void selectImage() async {
-      Uint8List? im = await pickImage(ImageSource.gallery);
-      setState(() {
-        _image = im;
-      });
-    }
-
-    void signUpUser() async {
-      setState(() {
-        isLoading = true;
-      });
-      String res = await AuthMethods().signUpUser(
+    // signup user using our authmethodds
+    String res = await AuthMethods().signUpUser(
         email: _emailController.text,
         password: _passwordController.text,
         username: _usernameController.text,
         bio: _bioController.text,
-        file: _image,
-      );
+        file: _image!);
+    // if string returned is sucess, user has been created
+    if (res == "success") {
       setState(() {
-        isLoading = false;
+        _isLoading = false;
       });
-      if (res != 'success') {
-        if (context.mounted) {
-          showSnackBar(res, context);
-          Navigator.of(context).push(MaterialPageRoute(
-            builder: (context) {
-              return const ResponsiveLayout(
-                webScreenLayout: WebScreenLayout(),
-                mobileScreenLayout: MobileScreenLayout(),
-              );
-            },
-          ));
-        }
+      // navigate to the home screen
+      if (context.mounted) {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: (context) => const ResponsiveLayout(
+              mobileScreenLayout: MobileScreenLayout(),
+              webScreenLayout: WebScreenLayout(),
+            ),
+          ),
+        );
       }
-
-      if (kDebugMode) {
-        print("Done :$res");
+    } else {
+      setState(() {
+        _isLoading = false;
+      });
+      // show the error
+      if (context.mounted) {
+        showSnackBar(
+          res,
+          context,
+        );
       }
     }
+  }
 
+  selectImage() async {
+    Uint8List im = await pickImage(ImageSource.gallery);
+    // set state because we need to display the image we selected on the circle avatar
+    setState(() {
+      _image = im;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: SafeArea(
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 32),
-            height: size.height,
-            width: double.infinity,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Flexible(
-                  flex: 2,
-                  child: Container(),
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 32),
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Flexible(
+                flex: 2,
+                child: Container(),
+              ),
+              SvgPicture.asset(
+                'assets/ic_instagram.svg',
+                color: primaryColor,
+                height: 64,
+              ),
+              const SizedBox(
+                height: 64,
+              ),
+              Stack(
+                children: [
+                  _image != null
+                      ? CircleAvatar(
+                          radius: 64,
+                          backgroundImage: MemoryImage(_image!),
+                          backgroundColor: Colors.red,
+                        )
+                      : const CircleAvatar(
+                          radius: 64,
+                          backgroundImage: NetworkImage(
+                              'https://i.stack.imgur.com/l60Hf.png'),
+                          backgroundColor: Colors.red,
+                        ),
+                  Positioned(
+                    bottom: -10,
+                    left: 80,
+                    child: IconButton(
+                      onPressed: selectImage,
+                      icon: const Icon(Icons.add_a_photo),
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              TextFieldInput(
+                hintText: 'Enter your username',
+                textInputType: TextInputType.text,
+                textEditingController: _usernameController,
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              TextFieldInput(
+                hintText: 'Enter your email',
+                textInputType: TextInputType.emailAddress,
+                textEditingController: _emailController,
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              TextFieldInput(
+                hintText: 'Enter your password',
+                textInputType: TextInputType.text,
+                textEditingController: _passwordController,
+                isPass: true,
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              TextFieldInput(
+                hintText: 'Enter your bio',
+                textInputType: TextInputType.text,
+                textEditingController: _bioController,
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              InkWell(
+                onTap: signUpUser,
+                child: Container(
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  decoration: const ShapeDecoration(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(4)),
+                    ),
+                    color: blueColor,
+                  ),
+                  child: !_isLoading
+                      ? const Text(
+                          'Sign up',
+                        )
+                      : const CircularProgressIndicator(
+                          color: primaryColor,
+                        ),
                 ),
-                SvgPicture.asset('assets/ic_instagram.svg',
-                    color: primaryColor, height: 64),
-                const SizedBox(height: 64),
-                Stack(
-                  children: [
-                    _image != null
-                        ? CircleAvatar(
-                            radius: 64,
-                            backgroundImage: MemoryImage(_image!),
-                          )
-                        : const CircleAvatar(
-                            radius: 64,
-                            backgroundImage: NetworkImage(
-                                "https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg"),
-                          ),
-                    Positioned(
-                      bottom: -10,
-                      left: 80,
-                      child: IconButton(
-                        icon: const Icon(Icons.add_a_photo),
-                        onPressed: () {
-                          selectImage();
-                        },
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Flexible(
+                flex: 2,
+                child: Container(),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: const Text(
+                      'Already have an account?',
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () => Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => const LoginScreen(),
                       ),
-                    )
-                  ],
-                ),
-                const SizedBox(height: 24),
-                TextFieldInput(
-                  textEditingController: _usernameController,
-                  hintText: 'Enter your username',
-                  textInputType: TextInputType.text,
-                ),
-                const SizedBox(height: 24),
-                TextFieldInput(
-                  textEditingController: _emailController,
-                  hintText: 'Enter your email',
-                  textInputType: TextInputType.emailAddress,
-                ),
-                const SizedBox(height: 24),
-                TextFieldInput(
-                  textEditingController: _passwordController,
-                  hintText: 'Enter your password',
-                  textInputType: TextInputType.text,
-                  isPass: true,
-                ),
-                const SizedBox(height: 24),
-                TextFieldInput(
-                  textEditingController: _bioController,
-                  hintText: 'Enter your bio',
-                  textInputType: TextInputType.text,
-                ),
-                const SizedBox(height: 24),
-                InkWell(
-                  onTap: signUpUser,
-                  child: Container(
-                    width: double.infinity,
-                    alignment: Alignment.center,
-                    padding: const EdgeInsets.symmetric(vertical: 12),
-                    decoration: const ShapeDecoration(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(4),
+                    ),
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(vertical: 8),
+                      child: const Text(
+                        ' Login.',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                      color: blueColor,
                     ),
-                    child: isLoading
-                        ? const Center(
-                            child:
-                                CircularProgressIndicator(color: primaryColor),
-                          )
-                        : const Text('Sign up'),
                   ),
-                ),
-                const SizedBox(height: 12),
-                Flexible(flex: 2, child: Container()),
-              ],
-            ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
